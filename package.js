f0307610306c8caf53a2d931e9413e81756904d4
oeper <82303578+oeper@users.noreply.github.com>
2024-10-16 22:34:13 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "oepernetarticles",
+  "version": "1.0.0",
+  "description": "Article website with guest posting and Google sign-in",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "firebase": "^9.15.0",
+    "firebase-admin": "^11.3.0",
+    "ejs": "^3.1.8"
+  }
+}
